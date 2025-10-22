2a4a078216d0ddc3c36a1f54036cd047068d1858
Anirban <anirban140963@gmail.com>
2025-10-23 00:52:06 +0530

    fully complete review page

--- backend/controllers/reviewControllers.js
+++ backend/controllers/reviewControllers.js
@@ -17,7 +17,7 @@ export const getReviews = async (req, res)=> {
     try {
         const reviews = await Review.find().sort({ createdAt: -1 });
         res.json(reviews);
-    } catch (error) {
-        res.status(500).json({ error: err.message });
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching reviews", error: err.message });
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from './config/db.js';
-import clubRoutes from './routes/clubRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+//import clubRoutes from './routes/clubRoutes.js';
+//import userRoutes from './routes/userRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js'
 import mongoose from 'mongoose';
 
@@ -16,14 +16,10 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 
-app.use("/api/clubs", clubRoutes);
-app.use("/api/users", userRoutes);
+//app.use("/api/clubs", clubRoutes);
+//app.use("/api/users", userRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-mongoose
-   .connect(process.env.MONGO_URI)
-   .then(()=> console.log("MongoDB connected"))
-   .catch((err)=> console.error("MongoDB connection error:", err));
 
 app.get("/", ( req, res) => {
     res.send("Backend working");
